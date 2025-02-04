0651305b355242ec28e4772c882d849d24474ace
Matthew Begun <matthew.begun@gmail.com>
2025-02-04 12:41:39 +1000

    Create init_wsl.bash

---
+++ init_wsl.bash
@@ -0,0 +1,17 @@
+# step 0 - copy .bashrc and .bash_aliases
+
+# system update
+up
+
+# python
+sudo apt install python3 ipython3
+
+# starship
+curl -sS https://starship.rs/install.sh | sh
+
+# node - nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+
+# node - nodjs LTS
+nvm install --lts
+
