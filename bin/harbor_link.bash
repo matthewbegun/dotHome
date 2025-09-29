a3ecd67ab47387531fcf66164f0d4e52c38c86f8
Matthew Begun <matthew.begun@gmail.com>
2025-09-29 13:18:16 +1000

    Delete bin/harbor_link.bash

--- bin/harbor_link.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# link.bash
-
-# link all the harbor shims - very manual version!
-ln -s ~/_harbor/loco/bin/loco.bash ~/.local/bin/loco
-ln -s ~/_harbor/ruhroh/bin/ruhroh.bash ~/.local/bin/ruhroh
