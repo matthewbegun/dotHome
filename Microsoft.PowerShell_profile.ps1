41f0a16df454d7b8cfdbc67afc912398f8d4dbd0
Matthew Begun <matthew.begun@gmail.com>
2026-03-03 09:49:27 +1000

    Clean up PowerShell profile by removing duplicates
    Removed duplicate import of the Microsoft.WinGet.CommandNotFound module.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -145,13 +145,11 @@ Set-Alias ipy ipython
 #endregion
 
 #region candy
+
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+
 # starship
 Invoke-Expression (&starship init powershell)
 #endregion
 
-#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
-
-Import-Module -Name Microsoft.WinGet.CommandNotFound
-#f45873b3-b655-43a6-b217-97c00aa0db58
-#endregion
 
